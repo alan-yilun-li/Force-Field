dbf4e469def8f0f287adde44bf2540e5a60962f6
alan-yilun-li <alan.yilun.li@gmail.com>
2016-12-29 17:08:50 -0500

    Added Basic Sprite Methods to initialize properties

--- src/Sprite.java
+++ src/Sprite.java
@@ -13,6 +13,45 @@ public class Sprite {
     private double height;
 
     
+    public Sprite()
+    {
+        xPos = 0;
+        yPos = 0;    
+        velocityX = 0;
+        velocityY = 0;
+    }
+
+    public void setImage(Image i)
+    {
+        image = i;
+        width = i.getWidth();
+        height = i.getHeight();
+    }
+
+    public void setImage(String filename)
+    {
+        Image i = new Image(filename);
+        setImage(i);
+    }
+
+    public void setPosition(double x, double y)
+    {
+        xPos = x;
+        yPos = y;
+    }
+
+    public void setVelocity(double x, double y)
+    {
+        velocityX = x;
+        velocityY = y;
+    }
+
+    public void addVelocity(double x, double y)
+    {
+        velocityX += x;
+        velocityY += y;
+    }
+    
     public void update(double time)
     {
         xPos += velocityX * time;
