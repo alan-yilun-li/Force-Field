24ac45c773b89788b2aa77b06250cdb517252ffe
alan-yilun-li <alan.yilun.li@gmail.com>
2016-12-29 16:42:28 -0500

    Added a Basic Sprite Class
    Added basic methods for the Sprite class: to update its position, to
    draw the sprite, and to check for intersections

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,4 +1,37 @@
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class Sprite {
 
+    private Image image;
+    private double xPos;
+    private double yPos;    
+    private double velocityX;
+    private double velocityY;
+    private double width;
+    private double height;
+
+    
+    public void update(double time)
+    {
+        xPos += velocityX * time;
+        yPos += velocityY * time;
+    }
+ 
+    public void render(GraphicsContext gc)
+    {
+        gc.drawImage( image, xPos, yPos );
+    }
+    
+    public Rectangle2D getBoundary()
+    {
+        return new Rectangle2D( xPos, yPos, width, height);
+    }
+ 
+    public boolean intersects(Sprite target)
+    {
+        return target.getBoundary().intersects( this.getBoundary() );
+    }
+ 
 }
